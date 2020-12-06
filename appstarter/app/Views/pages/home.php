a989748f615dda5fae0bf30b897218e3e911e20e
Mircea Casapu <mircea.casapu@gmail.com>
2020-12-07 00:22:59 +0200

    IDK wtf im doing

--- appstarter/app/Views/pages/home.php
+++ appstarter/app/Views/pages/home.php
@@ -1,3 +1,25 @@
 <section class="hero-bg">
-    <h1>Lalala <span class="typed"></span> </h1>
+    <div class="container">
+        <div class="jumbotron home-jumbo">
+            <span class="typed"></span>
+            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <hr class="my-4">
+            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            </p>
+        </div>
+    </div>
 </section>
+
+<script>
+    var typed = new Typed(".typed", {
+        strings: ["Web Developer^1000", "UI/UX Designer^1000"],
+        smartBackspace: true,
+        typeSpeed: 40,
+        backSpeed: 60,
+        cursorChar: "_",
+        loop: true,
+        fadeOut: true,
+    });
+</script>
